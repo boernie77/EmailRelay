0e0929d75f9939a357fb412dc0637aa49e90a846
boernie77 <115419572+boernie77@users.noreply.github.com>
2026-03-22 16:40:51 +0100

    Auto-update: 2026-03-22 16:40

--- alias-api/main.py
+++ alias-api/main.py
@@ -1,6 +1,8 @@
+import os
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from starlette.middleware.sessions import SessionMiddleware
 
 from database import init_db
 from routers import api, ui
@@ -13,6 +15,7 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(title="EmailRelay", lifespan=lifespan)
+app.add_middleware(SessionMiddleware, secret_key=os.getenv("API_SECRET", "changeme"))
 app.mount("/static", StaticFiles(directory="static"), name="static")
 app.include_router(api.router)
 app.include_router(ui.router)
